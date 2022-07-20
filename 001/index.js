3cc06225dfe755c14cfc456bc74a7e811f051b32
Kallel <102880413+KallelCHZS@users.noreply.github.com>
2022-07-19 23:15:25 -0300

    aula 1

---
+++ 001/index.js
@@ -0,0 +1,8 @@
+var nome = " Kallel Campos" 
+var idade = 26
+var frase = "Japão é o melhor time do mundo"
+alert("Meu primeiro comando! Bem vindo" + nome + " sua idade é " + idade)
+console.log(nome)
+console.log(idade)
+console.log(frase)
+console.log(frase.replace("Japão", "Brasil"))
